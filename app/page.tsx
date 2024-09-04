badaca43dc533b2e30dd3101440f463c4e4935bd
Your Name <you@example.com>
2024-09-04 16:42:35 -0400

    [CLEANUP]

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,15 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Kye Gomez
+        Kye Gomez's Home Page
       </h1>
       <p className="mb-4">
-        {`Hi, I'm Kye Gomez, and I build neural networks. 
-        I've been programming since I was 12 years old, and today, I lead Agora, an open-source AI research lab with over 8,200 researchers worldwide. 
+        {`Hellow, I'm Kye Gomez, and I build neural networks. 
+        I've been programming since I was 12 years old, and today, I lead Agora, an open-source AI research lab non-profit with over 8,200 researchers worldwide. 
         We've successfully trained thousands of models and continue to push the boundaries of AI innovation.
 
-        I'm also working on Swarms, a framework for orchestrating intelligent agents. If you're interested in AI research or neural networks, check out my work on GitHub and YouTube, where I share insights, projects, and tutorials.`}
+        I'm also working on Swarms, a framework for orchestrating millions of agents to automate recurring enterprise operations. 
+        If you're interested in AI research or neural networks, check out my work on GitHub and YouTube, where I share insights, projects, and tutorials.`}
       </p>
       <div className="my-8">
         {/* <BlogPosts /> */}
