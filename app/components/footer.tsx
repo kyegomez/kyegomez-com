291ad1c6952d1a5edf1aba870f26f15e42f4a892
Your Name <you@example.com>
2024-09-04 11:57:12 -0400

    [CLEANU[]

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -63,6 +63,17 @@ export default function Footer() {
             <p className="ml-2 h-7">Discord</p>
           </a>
         </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.youtube.com/@kyegomez3242"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">Youtube</p>
+          </a>
+        </li>
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
         © {new Date().getFullYear()} MIT Licensed
