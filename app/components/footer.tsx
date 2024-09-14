bca577e8169f31253d1a988e0f7d63393b7912d1
Your Name <you@example.com>
2024-09-13 23:15:42 -0400

    [CLEANUP]

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -19,7 +19,7 @@ export default function Footer() {
   return (
     <footer className="mb-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
-        <li>
+        {/* <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
@@ -29,7 +29,7 @@ export default function Footer() {
             <ArrowIcon />
             <p className="ml-2 h-7">rss</p>
           </a>
-        </li>
+        </li> */}
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
@@ -49,7 +49,7 @@ export default function Footer() {
             href="https://x.com/KyeGomezB"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">X</p>
+            <p className="ml-2 h-7">Twitter/X</p>
           </a>
         </li>
         <li>
