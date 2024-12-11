7956bbfdd54dfaeabdd6e97f093190b9a427a8f1
Kye Gomez <98760976+kyegomez@users.noreply.github.com>
2024-12-11 00:32:00 -0800

    Update posts.tsx

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -3,6 +3,7 @@ import { formatDate, getBlogPosts } from 'app/blog/utils'
 
 export function BlogPosts() {
   let allBlogs = getBlogPosts()
+
   return (
     <div>
       {allBlogs
@@ -18,7 +19,7 @@ export function BlogPosts() {
           <Link
             key={post.slug}
             className="flex flex-col space-y-1 mb-4"
-            href={/blog/${post.slug}}
+            href={`/blog/${post.slug}`}
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
               <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
